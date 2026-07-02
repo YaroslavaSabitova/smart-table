477793fe2ee54e0678e8bfda5b2807ff01c7e2dd
Yaroslava Sabitova <slava.sabitova@yandex.ru>
2026-07-02 14:00:03 +0300

    step 1

--- src/components/table.js
+++ src/components/table.js
@@ -1,4 +1,4 @@
-import {cloneTemplate} from "../lib/utils.js";
+import { cloneTemplate } from '../lib/utils.js';
 
 /**
  * Инициализирует таблицу и вызывает коллбэк при любых изменениях и нажатиях на кнопки
@@ -8,18 +8,67 @@ import {cloneTemplate} from "../lib/utils.js";
  * @returns {{container: Node, elements: *, render: render}}
  */
 export function initTable(settings, onAction) {
-    const {tableTemplate, rowTemplate, before, after} = settings;
-    const root = cloneTemplate(tableTemplate);
+  const { tableTemplate, rowTemplate, before, after } = settings;
+  const root = cloneTemplate(tableTemplate);
 
-    // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
+  // @todo: #1.2 —  вывести дополнительные шаблоны до и после таблицы
 
-    // @todo: #1.3 —  обработать события и вызвать onAction()
+  if (before) {
+    before.reverse().forEach(subName => {
+      // перебираем нужный массив идентификаторов
+      root[subName] = cloneTemplate(subName); // клонируем и получаем объект, сохраняем в таблице
+      root.container.prepend(root[subName].container); // добавляем к таблице после (append) или до (prepend)
+    });
+  }
 
-    const render = (data) => {
-        // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
-        const nextRows = [];
-        root.elements.rows.replaceChildren(...nextRows);
-    }
+  if (after) {
+    after.forEach(subName => {
+      // перебираем нужный массив идентификаторов
+      root[subName] = cloneTemplate(subName); // клонируем и получаем объект, сохраняем в таблице
+      root.container.append(root[subName].container); // добавляем к таблице после (append) или до (prepend)
+    });
+  }
 
-    return {...root, render};
+  // @todo: #1.3 —  обработать события и вызвать onAction()
+  root.container.addEventListener('change', () => {
+    onAction();
+  });
+
+  root.container.addEventListener('reset', () => {
+    setTimeout(() => {
+      onAction();
+    });
+  });
+
+  root.container.addEventListener('submit', e => {
+    e.preventDefault();
+    onAction(e.submitter);
+  });
+
+  const render = data => {
+    // @todo: #1.1 — преобразовать данные в массив строк на основе шаблона rowTemplate
+
+    //   берём новый массив data из data.js;
+    const nextRows = data.map(item => {
+      // клонированный шаблон строки
+      const row = cloneTemplate(rowTemplate);
+
+      //   console.log('data', data);
+      //   console.log('row', row);
+
+      // Трансформируем данные в строки и выводим их
+      Object.keys(item).forEach(key => {
+        if (key in row.elements) {
+          row.elements[key].textContent = item[key];
+        }
+      });
+
+      //   console.log('row2', row);
+      return row.container;
+    });
+
+    root.elements.rows.replaceChildren(...nextRows);
+  };
+
+  return { ...root, render };
 }
--- src/data.js
+++ src/data.js
@@ -1,14 +1,35 @@
-import {makeIndex} from "./lib/utils.js";
+import { makeIndex } from './lib/utils.js';
 
 export function initData(sourceData) {
-    const sellers = makeIndex(sourceData.sellers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const customers = makeIndex(sourceData.customers, 'id', v => `${v.first_name} ${v.last_name}`);
-    const data = sourceData.purchase_records.map(item => ({
-        id: item.receipt_id,
-        date: item.date,
-        seller: sellers[item.seller_id],
-        customer: customers[item.customer_id],
-        total: item.total_amount
-    }));
-    return {sellers, customers, data};
+  //   console.log('sourceData', sourceData);
+  // sourceData = dataset_1.js;
+
+  const sellers = makeIndex(
+    //   выбираем продавцов по id
+    sourceData.sellers,
+    'id',
+    v => `${v.first_name} ${v.last_name}`
+  );
+
+  //   выбираем покупателей по id
+  const customers = makeIndex(
+    sourceData.customers,
+    'id',
+    v => `${v.first_name} ${v.last_name}`
+  );
+
+  //   новый массив на основе 230 чеков из purchase_records;
+  const data = sourceData.purchase_records.map(item => ({
+    id: item.receipt_id,
+    date: item.date,
+    seller: sellers[item.seller_id],
+    customer: customers[item.customer_id],
+    total: item.total_amount,
+  }));
+
+  //   console.log('sellers', sellers);
+  //   console.log('customers', customers);
+  //   console.log('data', data);
+
+  return { sellers, customers, data };
 }
